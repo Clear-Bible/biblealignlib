d3af099f7460448021fb736c8fdefc90810d44e9
Sean Boisen <sean.boisen@biblica.com>
2026-03-11 08:05:34 -0700

    don't compute diffs, supply them
    comments on attributes

--- biblealignlib/util/__init__.py
+++ biblealignlib/util/__init__.py
@@ -3,25 +3,29 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
-from ..burrito import VerseData
-from ..burrito.VerseData import DiffRecord
+from ..burrito import DiffRecord, VerseData
 
 
 @dataclass
 class BCVPair:
     """Manage BCV data from two managers."""
 
+    # like "41004003"
     bcv: str
+    # VerseData for the first manager
     mgr1_data: Optional[VerseData] = None
+    # VerseData for the second manager
     mgr2_data: Optional[VerseData] = None
+    # a label for what data is available to the pair: "both", "mgr1",
+    # "mgr2", or "neither"
     pairing: str = ""
-    diffs: list[DiffRecord] = field(init=False, default_factory=list)
+    # a general list of differences
+    diffs: list[DiffRecord] = field(default_factory=list)
 
     def __post_init__(self) -> None:
         """Initialize an instance."""
         if self.mgr1_data and self.mgr2_data:
             self.pairing = "both"
-            self.diffs = self.mgr1_data.diff(self.mgr2_data)
         elif self.mgr1_data:
             self.pairing = "mgr1"
         elif self.mgr2_data:
